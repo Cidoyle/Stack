bb6b7525cfd42d7486f5c6295dbbe0224f37b0c9
Ciaran Doyle <10362459@mydbs.ie>
2018-04-23 21:08:05 +0100

    Btnx2

--- Stack/Form1.Designer.cs
+++ Stack/Form1.Designer.cs
@@ -32,6 +32,8 @@
             this.lblNexti = new System.Windows.Forms.Label();
             this.btnPush = new System.Windows.Forms.Button();
             this.btnPop = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtBox
@@ -70,11 +72,31 @@
             this.btnPop.UseVisualStyleBackColor = true;
             this.btnPop.Click += new System.EventHandler(this.btnPop_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(85, 174);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(341, 174);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(487, 160);
+            this.ClientSize = new System.Drawing.Size(529, 296);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnPop);
             this.Controls.Add(this.btnPush);
             this.Controls.Add(this.lblNexti);
@@ -92,6 +114,8 @@
         private System.Windows.Forms.Label lblNexti;
         private System.Windows.Forms.Button btnPush;
         private System.Windows.Forms.Button btnPop;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
